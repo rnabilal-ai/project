0af8967d052c05f0613f1d6ce28b34b3bb74affc
rnabilal-ai <rifqinabilal25@gmail.com>
2024-11-13 14:51:31 +0700

    Create index.php
    Menambahkan Index

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<?php
+    include_once('navbar.php');
+?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+            <div class="card-header text-center">
+                <h3>Sistem Infomasi Mahasiswa</h3>
+            </div>
+            <div class="card-body">
+                <h4>Selamat Datang, User</h4>
+                <p>Kata-kata hari ini</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
