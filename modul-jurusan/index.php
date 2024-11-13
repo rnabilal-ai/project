54dacd16745ef458e1f6c0fd52bddd84b6de4a98
rnabilal-ai <rifqinabilal25@gmail.com>
2024-11-13 15:18:24 +0700

    memperbaiki link navbar dan jurusan

--- modul-jurusan/index.php
+++ modul-jurusan/index.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beranda</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Data Jurusan</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
 </head>
 <body>
 <?php
-    include_once('navbar.php');
+    include_once('../navbar.php');
 ?>
 
 <div class="container">
     <div class="row mt-5">
         <div class="col-6 m-auto">
             <div class="card">
-            <div class="card-header">
-                <h3>Sistem Infomasi Mahasiswa</h3>
+            <div class="card-header text-center">
+                <h3>Data Jurusan</h3>
             </div>
             <div class="card-body">
                 <h4>Selamat Datang, User</h4>
@@ -28,6 +28,6 @@
 </div>
     
     <script src="js/bootstrap.js"></script>
-    <script src="js/bootstrap.bundle.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
 </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -1,3 +1,4 @@
+
 <nav class="navbar navbar-expand-lg bg-dark-subtle">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">SIM</a>
@@ -7,13 +8,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+          <a class="nav-link active" aria-current="page" href="/Maxx/project/index.php">Beranda</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Data Mahasiswa</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Data Jurusan</a>
+          <a class="nav-link active" aria-current="page" href="/Maxx/project/modul-jurusan/index.php">Data Jurusan</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Data Dosen</a>
