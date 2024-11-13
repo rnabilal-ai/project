53a0db1ce12513f1ac6e5f861456c6f5ae9266aa
rnabilal-ai <rifqinabilal25@gmail.com>
2024-11-13 16:51:42 +0700

    penyesuaian tampilan

--- modul-jurusan/index.php
+++ modul-jurusan/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Jurusan</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/all.css">
 </head>
 <body>
 <?php
@@ -13,21 +14,43 @@
 
 <div class="container">
     <div class="row mt-5">
-        <div class="col-6 m-auto">
+        <div class="col-8 m-auto">
             <div class="card">
-            <div class="card-header text-center">
-                <h3>Data Jurusan</h3>
+            <div class="card-header">
+                <h3 class="float-start">Data Jurusan</h3>
+                <span class="float-end"><a class="btn btn-primary"><i class="fa-solid fa-plus"></i>Tambah data</a></span>
             </div>
             <div class="card-body">
-                <h4>Selamat Datang, User</h4>
-                <p>Kata-kata hari ini</p>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Kode</th>
+                        <th scope="col">Nama Jurusan</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>
+                            <a class="btn btn-info btn-sm" href=""><i class="fa-solid fa-pen-to-square"></i></a>
+                            <a class="btn btn-danger btn-sm" href=""><i class="fa-solid fa-trash"></i></a>
+
+                        </td>
+                    </tr>
+                </tbody>
+                </table>
             </div>
             </div>
         </div>
     </div>
 </div>
     
-    <script src="js/bootstrap.js"></script>
+    <script src="../js/bootstrap.js"></script>
     <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/all.js"></script>
 </body>
 </html>
