85be12c4a04861536f94928d197c371a17f06605
rnabilal-ai <rifqinabilal25@gmail.com>
2024-12-04 17:11:36 +0700

    ini

--- modul-dosen/index.php
+++ modul-dosen/index.php
@@ -71,7 +71,7 @@
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    Yakin data <b><?=$data['nama ']?></b> ingin dihapus?
+                                    Yakin data <b><?=$data['nama']?></b> ingin dihapus?
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
